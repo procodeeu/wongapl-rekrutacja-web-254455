ac127bf27e4cd412b1953209ba5ad063cfa83d31
procodeeu <124219210+procodeeu@users.noreply.github.com>
2025-06-14 20:38:54 +0200

    add visual tests

---
+++ tests/visual.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Visual Regression tests', () => {
+  test('homepage should look correct', async ({ page }) => {
+    await page.goto('/');
+    await expect(page).toHaveScreenshot('homepage.png', {
+      fullPage: true,
+    });
+  });
+
+  test('our projects page should look correct', async ({ page }) => {
+    await page.goto('/nasze-projekty/1');
+    await expect(page).toHaveScreenshot('our-projects.png', {
+      fullPage: true,
+    });
+  });
+
+  test('project detail page should look correct', async ({ page }) => {
+    await page.goto('/nasze-projekty/1');
+    const firstProjectLink = page.locator('.news-item h2 a').first();
+    await firstProjectLink.click();
+    await expect(page.locator('.article-header h1')).toBeVisible();
+    await expect(page).toHaveScreenshot('project-detail.png', {
+      fullPage: true,
+    });
+  });
+});
