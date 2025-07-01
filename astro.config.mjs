6f7b194c3e79a618bba32e345858204e91e27de6
Lenovo <procodeoffice@gmail.com>
2025-07-01 09:01:55 +0200

    chore: remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://8e1f8d4e.wongapl-rekrutacja-web-254455-amy.pages.dev',
   output: 'static',
-  adapter: cloudflare(),
   integrations: [sitemap()],
 });
